67807280de3c1e204d89670d32dedb6040b13bd8
Colin <mcbolin@hotmail.com>
2024-06-22 14:06:27 +1000

    fix: validate on empty movies

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,8 +6,10 @@ import MovieCard from "./MovieCard.jsx";
 
 let favouriteMovies = [
     "Aliens",
+    " ",
     "Dune",
     "Anchorman",
+    "",
     "Shrek 2",
     "Blade Runner",
     "Lord of the Rings"
@@ -18,6 +20,19 @@ export default function MovieList() {
     return (
         <section>
             {favouriteMovies.map((movie, index) => {
+                // for React you must use === not ==
+                // if (movie && movie.length === false) {
+
+                // }
+
+                // if (movie.length !== 0){
+                //     return <MovieCard key={index + "-" + movie} bobsBurgers={movie}/>
+                // }
+                
+                if (movie.length === 0){
+                    return null;
+                }
+                
                 return <MovieCard key={index + "-" + movie} bobsBurgers={movie}/>
 
             })}
