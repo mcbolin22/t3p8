8a6b9976d42d20207f414ba0df475f125f8cc498
Colin <mcbolin@hotmail.com>
2024-06-22 10:23:33 +1000

    feat: render list of movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+let favouriteMovies = [
+  "Aliens",
+  "Dune",
+  "Anchorman",
+  "Shrek 2",
+  "Blade Runner",
+  "Lord of the Rings"
+];
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello World!</h1>
+        <h1>Favourite Movies</h1>
+        <ul>
+          { favouriteMovies.map((movie, index) => {
+            return <li key={movie + "-" + index}>
+              {movie}
+            </li>
+          })}
+        </ul>
+        
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
